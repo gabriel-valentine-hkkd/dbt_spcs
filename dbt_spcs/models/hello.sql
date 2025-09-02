ae570129a90e37d01b187559c89e2d7c0deb94b1
Gabriel_Valentine_HKKD <gabriel_valentine@hakkoda.io>
2025-09-02 16:14:30 -0600

    Update hello.sql

--- dbt_spcs/models/hello.sql
+++ dbt_spcs/models/hello.sql
@@ -1,16 +1,1 @@
--- models/hello.sql
-{{ config(
-    materialized = 'incremental',
-    unique_key   = 'run_date',                -- one row per day
-    on_schema_change = 'sync_all_columns'     -- auto-add new cols
-) }}
-
-select
-  current_date::date       as run_date,
-  current_timestamp()      as run_ts,
-  'hola desde dbt en SPCS' as message
-
-{% if is_incremental() %}
-  -- on subsequent runs, only insert rows newer than the max we already have
-  where current_date::date > (select coalesce(max(run_date),'1900-01-01') from {{ this }})
-{% endif %}
+select current_timestamp() as run_ts, 'hello' as message
