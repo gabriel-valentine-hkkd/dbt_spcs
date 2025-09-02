32cff1940bc8a0fec2f5b4ee5723fab097d4bda9
Gabriel_Valentine_HKKD <gabriel_valentine@hakkoda.io>
2025-09-02 16:13:54 -0600

    Update greet_now.sql

--- dbt_spcs/models/greet_now.sql
+++ dbt_spcs/models/greet_now.sql
@@ -1,15 +1,13 @@
--- models/greet_now.sql
 {{ config(
-    materialized = 'incremental',
-    unique_key   = 'run_date',
-    on_schema_change = 'sync_all_columns'
+  materialized = 'incremental',
+  unique_key   = 'run_date',
+  on_schema_change = 'sync_all_columns'
 ) }}
 
 select
   current_date::date  as run_date,
   current_timestamp() as run_ts,
-  'hola desde el modelo greet_now' as message
-
+  'greet_now'         as message
 {% if is_incremental() %}
   where run_date > (select coalesce(max(run_date),'1900-01-01') from {{ this }})
 {% endif %}
